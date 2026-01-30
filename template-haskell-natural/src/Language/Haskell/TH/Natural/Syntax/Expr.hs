0c6f10f5fc611eab8e8cb1cad974740a2e629922
Arthur Jamet <arthur.jamet@gmail.com>
2026-01-30 09:15:53 +0000

    TH: Exp: Remove type-level tracking of exp type for now, add let binding

--- template-haskell-natural/src/Language/Haskell/TH/Natural/Syntax/Expr.hs
+++ template-haskell-natural/src/Language/Haskell/TH/Natural/Syntax/Expr.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -6,18 +7,27 @@
 module Language.Haskell.TH.Natural.Syntax.Expr (
     -- * Types
     ExprDefinition,
-    -- ExprBuilder,
+
+    -- * Builder
+    ExprBuilder,
+    runExprBuilder,
+    liftQ,
+    unsafeWithBuilder,
+
+    -- * Operations
+    arg,
+    returns,
+    let_,
 ) where
 
-import Control.Lens (makeLenses, use, uses, views, (%~), (.=), (?=), (^.), (|>), (|>=))
+import Control.Lens (makeLenses, views, (?=), (^.), (|>=))
 import Control.Monad.Reader
 import Control.Monad.State
-import Data.Coerce
+import Data.Functor ((<&>))
 import Language.Haskell.TH (Q, TExp, mkName)
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Natural.Class (THBuilder, gen)
-import Language.Haskell.TH.Natural.Syntax.Internal hiding ((>>=))
-import qualified Language.Haskell.TH.Natural.Syntax.Internal.Builder as B
+import Language.Haskell.TH.Natural.Syntax.Internal hiding (liftQ, (>>=))
 import Language.Haskell.TH.Syntax (TExp (TExp))
 import Prelude hiding ((>>=))
 import qualified Prelude
@@ -26,13 +36,28 @@ type ExprDefinition a = Q (TExp a)
 
 data ExprBuilderState = MkEBS
     { _argNames :: [TH.Name]
-    , _binds :: [(Name, TH.Exp)]
+    , _lets :: [(TH.Name, TH.Exp)]
     , _returnedExp :: Maybe TH.Exp
     }
 
 makeLenses ''ExprBuilderState
 
-newtype ExprBuilder t prev next a = MkEB {unEB :: Builder ExprBuilderState prev next a} deriving (Functor)
+newtype ExprBuilder t prev next a = MkEB {unEB :: ConstBuilder ExprBuilderState a} deriving (Functor)
+
+runExprBuilder :: ExprBuilder t prev Ready () -> ExprDefinition t
+runExprBuilder (MkEB b) = do
+    st <- runBaseBuilder b (MkEBS [] [] Nothing)
+    let lamOrId = case st ^. argNames of
+            [] -> id
+            names -> TH.LamE (TH.VarP <$> names)
+    resExp <- case st ^. returnedExp of
+        Nothing -> fail "Missing returned expression"
+        Just e -> return e
+    let letDecs = st ^. lets <&> \(n, expr) -> TH.ValD (TH.VarP n) (TH.NormalB expr) []
+    return $ TExp $ lamOrId $ TH.LetE letDecs resExp
+
+unsafeWithBuilder :: ConstBuilder ExprBuilderState a -> ExprBuilder t prev next a
+unsafeWithBuilder = MkEB
 
 instance Applicative (ExprBuilder t step step) where
     pure a = MkEB $ pure a
@@ -51,32 +76,32 @@ instance MonadReader ExprBuilderState (ExprBuilder t step step) where
 liftQ :: Q a -> ExprBuilder t step step a
 liftQ q = MkEB $ lift q
 
-type family ConsumableArg a where
-    ConsumableArg (a ': b) = a
+arg :: forall curr t a. ExprBuilder t curr curr (TExp a)
+arg = unsafeWithBuilder $ do
+    prevArgCount <- views argNames length
+    let nextArgName = mkName $ 'a' : show (prevArgCount + 1)
+    argNames |>= nextArgName
+    return $ TExp $ TH.VarE nextArgName
 
-type family ConsumedArg a where
-    ConsumedArg (_ ': b) = b
+returns :: forall curr t t' b. (THBuilder b (TExp t')) => b -> ExprBuilder t curr Ready ()
+returns q = unsafeWithBuilder $ do
+    TExp expr <- lift $ gen @b @(TExp t') q
+    returnedExp ?= expr
 
-arg :: forall curr rest a b t. (ConsumableArg curr ~ a, ConsumedArg curr ~ b) => ExprBuilder t curr rest (TExp a)
-arg =
-    MkEB $
-        unsafeCastStep @curr @rest @curr @curr
-            ( Prelude.do
-                prevArgCount <- views argNames length
-                let nextArgName = mkName $ 'a' : show (prevArgCount + 1)
-                unsafeWithState $
-                    argNames |>= nextArgName
-                return $ TExp $ TH.VarE nextArgName
-            )
+let_ :: forall b t' t step. (THBuilder b (TExp t')) => b -> ExprBuilder t step step (TExp t')
+let_ b = unsafeWithBuilder $ do
+    prevLetCount <- views lets length
+    varName <- lift $ TH.newName ("var" ++ show prevLetCount)
+    TExp expr <- lift $ gen @b @(TExp t') b
+    lets |>= (varName, expr)
+    return $ TExp $ TH.VarE varName
 
-returns :: forall curr t t' b. (THBuilder b (TExp t'), curr ~ '[t']) => b -> ExprBuilder t curr '[] ()
-returns q =
-    MkEB $
-        unsafeCastStep @curr @'[] @curr @curr
-            ( Prelude.do
-                TExp expr <- lift $ gen @b @(TExp t') q
-                unsafeWithState $
-                    returnedExp ?= expr
-            )
-
--- TODO runExpBuilder, let_, do notation
+-- TODO do notation
+-- TODO Avoid having to do type application on gen
+--
+--
+-- type family ConsumableArg a where
+--     ConsumableArg (a ': b) = a
+--
+-- type family ConsumedArg a where
+--     ConsumedArg (_ ': b) = b
