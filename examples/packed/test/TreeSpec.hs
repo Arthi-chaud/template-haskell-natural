2d79f339b75c46fd60a8ff311d33abcb209b2c2c
Arthur Jamet <arthur.jamet@gmail.com>
2026-02-09 12:44:20 +0000

    TH: Test packed reimplementation

--- examples/packed/test/TreeSpec.hs
+++ examples/packed/test/TreeSpec.hs
@@ -8,9 +8,14 @@ import Data.Packed hiding (mkPacked)
 import Data.Packed.NaturalTH (mkPacked)
 import Test.Hspec
 
-data Tree a = Leaf a | Node (Tree a) (Tree a)
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Eq, Show)
 
 mkPacked ''Tree
 
 spec :: Spec
-spec = return ()
+spec = do
+    let tree = Node (Leaf 1) (Node (Leaf 2) (Leaf 3)) :: Tree Int
+    it "should pack/unpack tree" $
+        let packedTree = pack tree
+            (tree1, _) = unpack packedTree
+         in tree1 `shouldBe` tree
