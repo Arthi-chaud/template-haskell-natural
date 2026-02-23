e1129797b4040a1aa65fd69ad652c87d6a474367
Arthur Jamet <arthur.jamet@gmail.com>
2026-02-23 09:42:02 +0000

    Lens ADT: Haddock

--- lens-adt/src/Control/Lens/TH/ADT.hs
+++ lens-adt/src/Control/Lens/TH/ADT.hs
@@ -1,8 +1,31 @@
+-- | This module allows generating lenses for non-record ADTs.
+--
+-- To allow overloading (e.g. using the same lens name for multiple ADTs), it first generates a typeclass and then an instance for it.
+-- If other lenses generated in the same scope have the same name, only an instance is generated.
+--
+-- @
+-- data A = A Int
+-- data B = B Int
+--
+-- 'makeADTLenses' 'A ["value"]
+--
+-- -- ===> Generates the following
+-- class HasValue a b where
+--  value = 'Lens\'' a b
+--
+-- instance HasValue A Int where
+--  value = 'position' @1
+--
+-- 'makeADTLenses' 'B ["value"]
+--
+-- -- ===> Generates the following
+--
+-- instance HasValue B Int where
+--  value = 'position' @1
+-- @
 module Control.Lens.TH.ADT (
     makeADTLenses,
     makeADTLens,
-
-    -- * Internal
     lensClassName,
 ) where
 
@@ -14,7 +37,7 @@ import Data.List ((!?))
 import Data.Maybe
 import Language.Haskell.TH
 
--- | Generates lenses for all the fields of the given type.
+-- | Generates lenses for all the fields of the given type. Uses 'makeADTLens' for each field.
 makeADTLenses :: Name -> [String] -> DecsQ
 makeADTLenses tyName fieldNames =
     concat
@@ -23,9 +46,9 @@ makeADTLenses tyName fieldNames =
 -- | Generates a lens with the given name for the field at the given index for the given type
 --
 -- @
--- makeADTLens '(,) "left" 0
+-- 'makeADTLens' '(,) "left" 0
 --
--- -- Generates the following
+-- -- ===> Generates the following
 -- instance HasLeft (a,b) a where
 --  left = 'position' @1
 -- @
@@ -75,6 +98,7 @@ makeADTLensClass lensName =
     a = mkName "a"
     b = mkName "b"
 
+-- | Get the name of the lens typeclass to define, using the lens' name
 lensClassName :: Name -> Name
 lensClassName lensName = mkName $ "Has" ++ capitalize (nameBase lensName)
   where
